4dff2b2a7359255762d35d3964899ffecaef15cf
Matrosov <Matrosov@KOKSFENMETT>
2024-12-07 23:34:35 +0100

    Добавьте файлы проекта.

---
+++ №6(2) Python/_6_2__Python.py
@@ -0,0 +1,12 @@
+﻿
+def sum(): 
+    a = list(map(int, input('Enter your numbers: ').split()))
+    print("Начальний список: ", a)
+
+    s = 0
+
+    for i in range(len(a)): 
+        s += a[i]
+
+    print("Сума: ",s)
+sum()
